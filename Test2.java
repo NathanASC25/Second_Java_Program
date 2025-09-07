47c8f5f0df8c5ed874cb788716a0cd202e69ca05
NathanASC25 <javanathanjchin@gmail.com>
2025-09-07 02:11:58 -0400

    seventh commit

--- Test2.java
+++ Test2.java
@@ -5,7 +5,7 @@ public class Test2{
         Scanner scan = new Scanner(System.in);
         Random random = new Random();
         System.out.printf("\nI'm thinking of a number between 1 and 100\n(including both). Can you guess what it is?\n");
-        int computerNum = random.nextInt(1, 100);
+        int computerNum = random.nextInt(100) + 1;
         System.out.printf("\n%s", "Type a number: ");
         int guess = scan.nextInt();
         System.out.printf("%s %d\n","Your guess is:", guess);
